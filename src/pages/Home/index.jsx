b03140f029189899b27c015c2709b06d4c40ba27
Neogiciaa <chevalier.stephen@hotmail.com>
2023-07-10 10:02:24 +0200

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        Ceci es ma page Home et je suis dans le Header
+      </header>
+    </div>
+  );
+}
+
+export default App;
